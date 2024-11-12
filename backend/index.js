3db17c65746fa464e751e108b95b6a7effd97fc3
NWABUEZE AMARACHI <105382386+Amyy16@users.noreply.github.com>
2024-11-12 12:22:23 +0100

    Fixed cors error

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
+const cors = require("cors")
 const userRoute = require("./routes/user");
 dotenv.config();
- const cors = require("cors")
-
-app.use(cors())
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 const PORT = process.env.PORT || 5000;
 
 mongoose
